00ba21a3dc53ff4a8d4de4d3c0c3f1e8f7fdc644
Anastasia <124253381+ThiefVorey@users.noreply.github.com>
2023-04-29 11:21:27 +0700

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,32 @@
+class CATDATE():
+    def catname():
+        pass
+    def kitty():
+        pass
+    def location():
+        pass
+    def catinput():
+       pass
+    
+class VETDATE():
+
+    def volunteer():
+        pass
+    def employment():
+        pass
+    def nobus():
+        pass
+    def vetinput():
+        pass
+class treatment():
+    def svoboda():
+        pass
+    def badcat():
+        pass
+    def gear():
+        pass
+    def cure( ):
+        pass
+    def result():
+        pass
+
